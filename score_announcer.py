aa13fe9dcb6d690154c2f91616f5305643c34007
Anjana <58140600+anjana-kt@users.noreply.github.com>
2021-08-22 16:04:24 +0530

    Add files via upload

---
+++ score_announcer.py
@@ -0,0 +1,37 @@
+import pyttsx3
+
+high_score = 50.0
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'english_rp+f3')
+engine.setProperty('rate', 150)
+
+def highscoreAchieved():
+    high_score = score
+    print ("Your score is:",score,"\nThe high score is: ",high_score)
+    engine.say("Congragulations, It's a high score")
+    engine.say("Your score is: ",score)
+    engine.runAndWait()
+
+def notHighscore():
+    print ("Your score is:",score,"\nThe high score is: ",high_score)
+    engine.say("Better luck next time..")
+    engine.say("Your score is: ",score)
+    engine.runAndWait()
+
+arduino_input = int(input())
+Distance = int(input())
+score = round(Distance,2)
+
+while arduino_input == 1:
+
+    if score >= high_score:
+	    highscoreAchieved()
+    elif score < high_score:
+	    notHighscore()
+
+    arduino_input = int(input())
+    Distance = int(input())
+    score = round(Distance,2)
+
