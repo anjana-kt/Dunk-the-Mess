bce42820992d09ad2030f2152e606ad6b3ebbdb5
Anjana <58140600+anjana-kt@users.noreply.github.com>
2021-08-22 18:36:51 +0530

    Add files via upload

---
+++ covid19QA.py
@@ -0,0 +1,26 @@
+import csv
+import pyttsx3
+import random
+
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'english_rp+f3')
+
+fields=[]
+rows=[]
+
+with open('dataset.csv','r') as csvfile:
+    csvreader= csv.reader(csvfile)
+    fileds=next(csvreader)
+
+    for row in csvreader:
+        rows.append(row)
+    
+    n=csvreader.line_num
+    i=random.randint(0,n-1)
+
+for row in rows[i:i+1]:
+    for col in row:
+        print("%10s"%col)
+        break
